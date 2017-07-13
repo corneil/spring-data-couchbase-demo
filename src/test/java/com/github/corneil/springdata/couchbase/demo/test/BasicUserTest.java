7709f407a1836f9c790ef577340cacd0c4870bb1
corneil <8720cdp>
2017-07-13 15:51:24 +0200

    added check to verify object fields.

--- src/test/java/com/github/corneil/springdata/couchbase/demo/test/BasicUserTest.java
+++ src/test/java/com/github/corneil/springdata/couchbase/demo/test/BasicUserTest.java
@@ -146,5 +146,7 @@ public class BasicUserTest {
 		UserInfo loaded = userInfoRepository.findOne(id);
 		assertNotNull(loaded);
 		assertNotNull(loaded.getId());
+		assertEquals(user.getEmailAddress(), loaded.getEmailAddress());
+		assertEquals(user.getDateOfBirth(), loaded.getDateOfBirth());
 	}
 }
